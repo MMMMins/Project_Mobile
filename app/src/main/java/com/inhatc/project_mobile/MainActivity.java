859f431a8a4eb969c836268f6f6c91bb02e9dc1c
min <tommp1055@gmail.com>
2023-06-01 11:26:38 +0900

    친구추가구현 ㅠㅠ;

--- app/src/main/java/com/inhatc/project_mobile/MainActivity.java
+++ app/src/main/java/com/inhatc/project_mobile/MainActivity.java
@@ -5,13 +5,23 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -23,11 +33,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnLogin;
     private Button btnSignUpLoad;
     private ImageButton btnReset;
+    private FirebaseAuth auth;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         captchaImageView = findViewById(R.id.captchaImageView);
         GeneratorCaptcha.resetCaptcha();
@@ -53,26 +65,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v){
         if(v == btnLogin){
-            String userID   = edtUserID.getText().toString();
-            String userPwd  = edtUserPwd.getText().toString();
-            String userCode = edtUserCode.getText().toString();
-
             // 빈칸 검증
             if(edtIsEmpty(edtUserID)) return;
             if(edtIsEmpty(edtUserPwd)) return;
             if(edtIsEmpty(edtUserCode)) return;
 
+            String userID   = edtUserID.getText().toString();
+            String userPwd  = edtUserPwd.getText().toString();
+            String userCode = edtUserCode.getText().toString();
+
             // Captcha 인증 확인
             if(!userCode.equals(GeneratorCaptcha.getCaptchaCode())){
-                // 입력한 Captcha 값과 설정된 값이 틀리다면
-                /*
-
-                로그인 실패 로직 구현
-
-                */
                 // 값 초기화
-                edtUserID.setText("");
-                edtUserPwd.setText("");
                 edtUserCode.setText("");
 
                 // 팝업 메시지
@@ -84,7 +88,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 return;
             }
+
             //로그인 성공로직 구현
+            loginDB(userID, userPwd);
 
             //액티비티 전환
             Toast.makeText(this,edtUserID.getText().toString()+"님 반갑습니다.",Toast.LENGTH_SHORT).show();
@@ -105,6 +111,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    public void loginDB(String email, String password){
+        auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = auth.getCurrentUser();
+                        String uid = user.getUid();
+
+                        Intent mainAppLoad = new Intent(MainActivity.this, AppMainActivity.class);
+                        mainAppLoad.putExtra("UID", uid);
+                        startActivity(mainAppLoad);
+
+                        Toast.makeText(getApplicationContext(), email+"님 환영홥니다.", Toast.LENGTH_LONG).show();
+                        finish();
+                    } else {
+                        edtUserID.setText("");
+                        edtUserPwd.setText("");
+                        edtUserCode.setText("");
+                        edtUserID.requestFocus();
+                        Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
     public boolean edtIsEmpty(EditText userValue){
         if(userValue.getText().toString().isEmpty()){
             userValue.requestFocus();
--- app/src/main/java/com/inhatc/project_mobile/User.java
+++ app/src/main/java/com/inhatc/project_mobile/User.java
@@ -3,11 +3,20 @@ package com.inhatc.project_mobile;
 public class User {
     private String name;
     private String email;
+    private String uid;
 
+    public User(){}
     public User(String name, String email){
         this.name = name;
         this.email = email;
     }
+    public User(String name, String emali, String uid){
+        this.name = name;
+        this.email = emali;
+        this.uid = uid;
+    }
+
+
 
     public String getName() {
         return name;
@@ -23,5 +32,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 }
